34f8da0409679756136e335c76cfadd10c3d9aa7
etchlon <94682545+etchlon@users.noreply.github.com>
2023-01-08 08:14:53 -0500

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,14 +2,14 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
-       sh "mvn clean package"
+       bat "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh "mvn clean sonar:sonar"
+       bat "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
-       sh "mvn clean deploy"
+       bat "mvn clean deploy"
      }
 }
